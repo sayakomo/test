9f462b01be07e3115f13f5fc2bd547024409a35f
komo <skfgood2@outlook.com>
2026-09-04 06:15:26 +0700

    Create api/data.js

---
+++ api/data.js
@@ -0,0 +1,29 @@
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method Not Allowed"
+    });
+  }
+
+  const {
+    Type,
+    Versi,
+    Device
+  } = req.body || {};
+
+  return res.status(200).json({
+    FYF: [
+      {
+        ID: Versi || "",
+        Device: Device || "",
+        Versi: Type || "",
+        Admin: "Telegram @KopiNih",
+        Expired: "04-09-2026",
+        ON: true,
+        OFF: true,
+        BUS: "ca-app-pub-6654559110094293/5995432864",
+        MRT: "ca-app-pub-6654559110094293/6051273181#"
+      }
+    ]
+  });
+}
